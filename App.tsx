682b51dad3e64ae17c9d76805ba7e026ad8a9d81
Babus95 <111665038+Babus95@users.noreply.github.com>
2024-10-22 09:26:03 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <>
+      <div className="cont">
+        <img src="./img/lam-logo.png" className="logo" />
+        <h1>Abbos Egamberdiev</h1>
+        <h2>Front-end Developer</h2>
+        <button className="button">Email</button>
+        <h1 className="about">About</h1>
+        <p>
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+        <h1 className="about">Interests</h1>
+        <p>
+          Food expert. Music scholar.Reader Internet fanatic.Beef. Enterpreneus
+          . Travel geek. Pop culture ninja. Coffee fanatic.
+        </p>
+        <img src="./img/Twitter Icon.png" />
+        <img src="./img/Facebook Icon.png" />
+        <img src="./img/Instagram Icon.png" />
+        <img src="./img/GitHub Icon.png" />
+      </div>
+    </>
+  );
+}
+
+export default App;
